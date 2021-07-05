9dde413318856e322f51898a1153a48f60f18e03
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-05 19:11:13 +0800

    Added variables

--- tutorial-2.py
+++ tutorial-2.py
@@ -1,1 +1,27 @@
-# variables
+# variables & variable name
+# Core component to store data
+
+# int
+students_count = 1000
+print(students_count)
+
+# decimal
+rating = 4.99
+print(rating)
+
+# boolean
+is_published = False
+print(is_published)
+
+# string
+course_name = "Python Programming"
+print(course_name)
+
+
+# Best practices to write somee variable name
+
+# 1. Should be decriptive and meaningful
+# 2. Avoid using mystical names for the variables
+# 3. All variable should be in small variables
+# 4. Use underscore for multiple words
+# 5. Encourage to add some spacing to make it spacious and readable
