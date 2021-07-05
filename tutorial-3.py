9833de410bf7a37c2f0f3d5456c722f9ba88d446
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-05 19:28:33 +0800

    Added strings

--- tutorial-3.py
+++ tutorial-3.py
@@ -1,1 +1,25 @@
 # strings
+
+course = "Python Programming"
+
+# len(): get the length of a string
+print(len(course))
+
+# []: to access specific element or specific character
+# : : to slice a string
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+
+
+# triple quotes (Optional)
+message = """
+ Hi John,
+
+ This is Mosh from codewithmosh.com
+
+ Blah blah blah
+"""
