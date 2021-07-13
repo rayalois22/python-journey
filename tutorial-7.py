6dab55f13ba93cb19bb8aa7004a09352df24365e
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-13 21:54:52 +0800

    Add numbers

---
+++ tutorial-7.py
@@ -0,0 +1,46 @@
+# numbers
+
+# integer
+x = 1
+
+# decimal
+x = 1.1
+
+# complex number
+x = 1 + 2j
+
+# addition
+print(10 + 3)
+
+# subtraction
+print(10 - 3)
+
+# multiplication
+print(10 * 3)
+
+# division - final result will get a floating point number
+print(10 / 3)
+
+# division - final result will be an integer
+print(10 // 3)
+
+# modulus
+print(10 % 3)
+
+# exponent - left to the power of right
+print(10 ** 3)
+
+# augmented assignment operator
+# x = 10
+# x = x + 3
+# x += 3
+
+# OUTPUT
+#
+# 13
+# 7
+# 30
+# 3.3333333333333335
+# 3
+# 1
+# 1000
