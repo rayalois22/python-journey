56d59020ab21c2737b291869251277ea99331560
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-13 19:43:51 +0800

    Changes for tutorial-5 and Added String Methods

--- tutorial-5.py
+++ tutorial-5.py
@@ -3,5 +3,14 @@
 first = "Mosh"
 last = "Hamedani"
 # full = first + " " + last
-full = f"{len(first)} {last}"
+# full = f"{first} {last}"
+# full = f"{len(first)} {last}"
+full = f"{len(first)} {2 + 2}"
 print(full)
+
+# OUTPUT
+#
+# Mosh Hamedani
+# Mosh Hamedani
+# 4 Hamedani
+# 4 4
