8a0ccb4e324ade29aa6c12c4af1309306d7b3db9
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-14 15:29:31 +0800

    Added working with numbers

--- tutorial-8.py
+++ tutorial-8.py
@@ -1,1 +1,22 @@
 # working with numbers
+
+# math module - use for complex mathematical calculations
+import math
+
+# rounding number
+print(round(2.9))
+
+# return absolute value of a number
+print(abs(-2.9))
+
+# example of math module but it has a lot of functions
+# Refer this link which has provided a complete list of the functions for the math module, https://docs.python.org/3/library/math.html
+
+# first example - getting the ceiling of a number
+print(math.ceil(2.2))
+
+# pi
+math.pi
+
+# cosine of the value of radian
+print(math.cos(12))
