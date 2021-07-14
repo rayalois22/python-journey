304d7173523e685de73dec7974d742296ff8b17c
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-14 20:39:41 +0800

    Add conditional statements

---
+++ tutorial-10.py
@@ -0,0 +1,20 @@
+# conditional statements
+
+temperature = 15
+
+# if
+# don't forget to include : for if statement so that it will go to the next line with 2 or 4 white spaces
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+
+# elif is short form of else if
+# don't forget to include : for elif statement so that it will go to the next line with 2 or 4 white spaces
+elif temperature > 20:
+    print("It's nice")
+
+# else
+# don't forget to include : for else statement so that it will go to the next line with 2 or 4 white spaces
+else:
+    print("It's cold")
+print("Done")
