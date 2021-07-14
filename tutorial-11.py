08e19a7c1c7577721c26164663d0551dfc6a0c36
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-14 20:50:09 +0800

    Add ternary operator

---
+++ tutorial-11.py
@@ -0,0 +1,12 @@
+# ternary operator
+
+age = 22
+
+# if age >= 18:
+#     message = "Eligible"
+# else:
+#     message = "Not eligible"
+
+# cleaner way to write ternary operator
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
