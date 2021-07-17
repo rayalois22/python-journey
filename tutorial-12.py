654b7703fb7256b018155c7ca12b842deeccb7cb
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-17 19:02:16 +0800

    Update tutorial-12.py

--- tutorial-12.py
+++ tutorial-12.py
@@ -1,1 +1,45 @@
 # logical operator
+
+# and, or, not operator
+
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+# and operator
+#
+# high_income = False
+# good_credit = True
+#
+# if high_income and good_credit:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+# or operator
+#
+# high_income = False
+# good_credit = True
+#
+# if high_income or good_credit:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+# not operator
+#
+# high_income = False
+# good_credit = True
+# student = False
+#
+# if not student:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
