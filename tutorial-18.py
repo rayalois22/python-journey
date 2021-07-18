f9ec8a9d942f3c77dd7c5885ec9a675a91a84151
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-18 15:19:03 +0800

    Add iterables

---
+++ tutorial-18.py
@@ -0,0 +1,43 @@
+# Iterables
+
+# string
+#
+# for x in "Python":
+#     print(x)
+#
+# OUTPUT:
+#
+# P
+# y
+# t
+# h
+# o
+# n
+
+# list
+#
+for x in [1, 2, 3, 4]:
+    print(x)
+#
+# OUTPUT:
+#
+# 1
+# 2
+# 3
+# 4
+
+# custom list
+#
+# shopping_cart = ["flour", "oil", "maggie", "vegetable", "salad", "soy sauce"]
+#
+# for item in shopping_cart:
+#     print(item)
+#
+# OUTPUT:
+#
+# flour
+# oil
+# maggie
+# vegetable
+# salad
+# soy sauce
