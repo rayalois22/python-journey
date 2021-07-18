2d4dc2f3860912dcb32a9496b900e00555c51dde
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-18 17:27:02 +0800

    Add control flow exercise

---
+++ control-flow.py
@@ -0,0 +1,40 @@
+# exercises
+
+# OUTPUT:
+# 2
+# 4
+# 6
+# 8
+# We have 4 even numbers
+# HINT: USE range(1, 10)
+#
+# ANSWER:
+#
+count = 0
+
+for numbers in range(1, 10):
+    if numbers % 2 == 0:
+        count += 1
+        print(numbers)
+print(f"We have {count} even numbers")
+
+
+# heckerio's answer
+#
+# for numbers in range(1, 10):
+#     print(numbers * 2)
+# else:
+#     print("We have", numbers, "even numbers")
+#
+# OUTPUT:
+#
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# We have 9 even numbers
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-    number //= 2
+    # numbers += numbers
