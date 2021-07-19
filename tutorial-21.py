d870d95638b28a2b326818ef635b14dcfa7e3997
Henry Law JunWei <65068770+heckerio@users.noreply.github.com>
2021-07-19 14:57:11 +0800

    Add defining functions

---
+++ tutorial-21.py
@@ -0,0 +1,17 @@
+# defining function
+
+# ways to define a variable inside the function
+# make sure that it's
+# - meaningful,
+# - descriptive,
+# - use lower case letterr to name the function
+# -use an underscore to separate multiple words
+
+def greet():
+    print("Hi there")
+    print("Welcome aboard")
+
+
+# Add two line breaks after this function (pep8 recommends to keep our code clean and maintainable)
+# If we didn't add two line breaks, the autopep8 will auto generate for us once we save the files
+greet()
