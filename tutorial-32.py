656142e1a98de221e6286753bfde13d94827cf87
hezagon <hezagon99@gmail.com>
2021-08-05 15:11:49 +0800

    Add accessing items

---
+++ tutorial-32.py
@@ -0,0 +1,26 @@
+# accessing items
+
+letters = ["a", "b", "c", "d"]
+letters[0] = "A"
+print(letters[0:3])
+print(letters[:3])
+print(letters[0:])
+print(letters[:])
+print(letters)
+
+# OUTPUT
+#
+# ['A', 'b', 'c']
+# ['A', 'b', 'c']
+# ['A', 'b', 'c', 'd']
+# ['A', 'b', 'c', 'd']
+# ['A', 'b', 'c', 'd']
+
+numbers = list(range(20))
+print(numbers[::2])
+print(numbers[::-1])
+
+# OUTPUT
+#
+# [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+# [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
